352b4562a1a9dc4041f61c31f9b32da8a3dc8be2
Brian Anderson <banderson623@gmail.com>
2017-09-14 14:10:59 -0500

    Just moving all of this to use directories.

---
+++ exercise-javascript-logging/README.md
@@ -0,0 +1,72 @@
+# Javascript Logging Exercise
+
+During the big rewrite of the Artisanal Mayonnaise website, [tastymayo.yum](http://tastymayo.yum), you realize there are many different front end components. Tracking their state via `console.log()` is proving to be a hot mess of thousands of debug logs being emitted in the console on every page reload.  There has got to be a better way to log all of these states without littering the console.
+
+You devise a plan. And decide to create a small library to record these logs without printing them to the console.
+
+Initially, it has the following interface available.
+
+```js
+MayoLog.log('Mayonnaise bottle 1 displayed ');
+// and, optionally with an arbitrary dataset
+MayoLog.log('Mayonnaise bottle 3 displayed', {mayoFlavId: 12});
+```
+
+As you progress you realize you need to find a way to retrieve items from the log, so you add in the following:
+
+```js
+// will print all of the logged events to the console
+MayoLog.toConsole();
+// in something like the following format:
+// (103ms) Mayonnaise bottle 3 displayed {mayoFlavId: 12}
+```
+
+Oh yeah, you realized it would be nice to record the time, in milliseconds, from [DOMContentLoaded](https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded) to when this log entry was added. That might help later on when debugging mega-mayonnaise performance concerns.
+
+As you continue to add more logs, you realize that what you really need is a way to filter these logs. So you decide to add the following capabilities to filter on the data attribute.
+
+```js
+// This will return all log entries that have the
+// attribute 'mayoFlavId' in their dataset
+MayoLog.findWithDataAttribute('mayoFlavId');
+
+// returning something like the following
+[
+  {
+   time: 103,
+   log: 'Mayonnaise bottle 3 displayed',
+   data: {mayoFlavId: 12}
+  },
+  {
+   time: 144,
+   log: 'Bottle 5 clicked',
+   data: {mayoFlavId: 52, size: 'Largest'}
+  }
+]
+```
+
+Feeling mighty proud of yourself, you begin to get ambitious and wonder about how you might transmit these logs to your server for 1% of your mayonnaise loving traffic?
+
+For this you design a function, that can take a URL, and when called will `POST` the entire log as a `JSON` object to this endpoint. _And remember, this should only be called for 1% of your traffic, we don't want to melt your mayo server._
+
+```js
+MayoLog.sendToServer('http://tastymayo.yum/analytics');
+```
+
+* * *
+
+As a recap you should now have a nice logging library that has the following interface:
+
+```js
+MayoLog.log(<string>)
+MayoLog.log(<string>, <object>)
+MayoLog.toConsole()
+MayoLog.findWithDataAttribute(<string>)
+MayoLog.sendToServer(<url>)
+```
+
+As a convenience, we've uploaded [a starter file](https://gist.github.com/banderson623/c20d7b3adc023c5ce3404d2efde95bdb)  with a single function to help you generate many log entries.
+
+```js
+loadSampleLogEntries(100);
+```
---
+++ exericse-ruby-string-replacement/swirl_test.rb
@@ -0,0 +1,107 @@
+# Hey boss, make sure you include your Swirl library
+# Any that nice CashWords library too
+# require File.join(File.dirname(__FILE__),"swirl")
+# require File.join(File.dirname(__FILE__),"cash_words")
+require "test/unit"
+
+
+class TestSwirl < Test::Unit::TestCase
+  def test_basic_interface
+    swirl = Swirl.new
+
+    assert(swirl.is_a?(Swirl),
+          'Swirl should be able to be instantiated')
+
+    assert(swirl.respond_to?(:add_html),
+          'Swirl should be able to accept an html string via :add_html')
+
+    assert(swirl.respond_to?(:use_affiliate_database),
+          'Swirl should be able to use a database look up via :use_affiliate_database')
+
+    assert(swirl.respond_to?(:money_making_html),
+          'Swirl should return the final html via :money_making_html')
+  end
+
+  def test_without_a_database_no_money_making_changes_are_performed
+    sample_string = '<h1>Latest Craze, cheese smoothies!</h1>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    assert_equal sample_string, swirl.money_making_html
+  end
+
+  def test_with_a_database_money_making_changes_are_performed
+    sample_string = '<h1>Latest Craze, cheese smoothies!</h1>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(CashWords.new)
+    assert_not_equal sample_string, swirl.money_making_html
+  end
+
+  def test_money_making_changes_insert_a_link
+    sample_string = '<h1>Latest Craze, cheese smoothies!</h1>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(CashWords.new)
+    assert swirl.money_making_html.include?('href'),
+          'links must include an href attribute'
+  end
+
+  def test_money_making_can_handle_weird_cases
+    sample_string = '<h1>Latest Craze, CHEese smoothies!</h1>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(CashWords.new)
+    assert swirl.money_making_html.include?('href'),
+          'links must include an href attribute'
+  end
+
+  def test_money_making_can_handle_weird_cases_without_mangling_the_text
+    sample_string = '<h1>Latest Craze, CHEese smoothies!</h1>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(CashWords.new)
+    output = swirl.money_making_html
+    assert output.include?('CHEese'),
+          'links must maintain their inner text formatting ('+output+')'
+  end
+
+  def test_money_making_changes_can_handle_multiple_changes
+    sample_string = '<p>Latest Craze, cheese smoothies and kale! </p>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(CashWords.new)
+    output = swirl.money_making_html
+
+    assert_equal 2, output.scan(/href/).count,
+                'links must include two links ('+output+')'
+  end
+
+  def test_will_pick_the_highest_affiliate_amount
+    sample_string = '<p>Latest Craze, cheese smoothies!</p>'
+    swirl = Swirl.new
+    swirl.add_html(sample_string)
+    swirl.use_affiliate_database(FakeCashWords.new)
+    output = swirl.money_making_html
+    assert_include output, 'bigmoney.com'
+  end
+
+  # Hey boss, I got stuck here, I'd really like to add a few tests
+  # to make sure we aren't mangling any of the HTML, but I wasn't sure
+  # how to do that ... also I am falling asleep
+  def test_wont_mangle_the_html
+    assert false, "I was too tired to write this, please help me."
+  end
+end
+
+
+# Boss, I wrote a mocking library to make sure we are selecting the
+# highest amount from our affiliates
+class FakeCashWords
+  def has_word?(word) word == 'cheese'; end
+  def get_links_for_word(word)
+    [
+      { url:'https://cheapo.com/food', amount: 0.001},
+      { url:'https://bigmoney.com/rottenstuff', amount: 0.02}
+    ]
+  end
+end
